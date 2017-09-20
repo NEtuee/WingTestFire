44383e341b289e79e55a1dcde756d117de59705b
Lee Umin <netuee@naver.com>
2017-09-21 02:03:54 +0900

    09_21_0

---
+++ wing/Assets/InputManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+
+	public PlayerAttack attack;
+	public GameObject player;
+
+	Touch touchs;
+
+	bool isTouched = false;
+	Vector2 playerOrigin;
+	Vector2 touchOrigin;
+
+	void Update () {
+		if(Input.touchCount > 0)
+		{
+			for(int i = 0; i < Input.touchCount; ++i)
+			{
+				touchs = Input.GetTouch(i);
+				if(touchs.position.x > Screen.width / 3)
+				{
+					if(touchs.phase == TouchPhase.Began)
+					{
+						Vector3 pos = Camera.main.ScreenToWorldPoint(touchs.position);
+						pos.z = 0;
+						attack.transform.position = pos;
+					}
+					else if(touchs.phase == TouchPhase.Moved)
+					{
+						Vector3 pos = Camera.main.ScreenToWorldPoint(touchs.position);
+						pos.z = 0;
+						attack.transform.position = pos;
+					}
+				}
+				else
+				{
+					if(touchs.phase == TouchPhase.Began)
+					{
+						playerOrigin = player.transform.position;
+						touchOrigin = Camera.main.ScreenToWorldPoint(touchs.position);
+					}
+					else if(touchs.phase == TouchPhase.Moved)
+					{
+						Vector2 pos = new Vector2(playerOrigin.x,playerOrigin.y - (touchOrigin.y - Camera.main.ScreenToWorldPoint(touchs.position).y));
+						player.transform.position = pos;
+					}
+				}
+			}
+		}
+	}
+}
---
+++ wing/Assets/PlayerAttack.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerAttack : MonoBehaviour {
+
+	public bool start = false;
+
+	public Text text;
+
+	Vector2 prevPos;
+	Vector2 currentPos;
+
+	RaycastHit2D ray;
+
+	void Update()
+	{
+		currentPos = transform.position;
+		if(start)
+		{
+			prevPos = transform.position;
+			start = false;
+		}
+
+		if(prevPos != currentPos)
+		{
+			Vector2 dir = currentPos - prevPos;
+			float dist = Vector2.Distance(prevPos,currentPos);
+			ray = Physics2D.Raycast(prevPos,dir.normalized,dist);
+			if(ray.collider != null)
+			{
+				Debug.Log(ray.collider.gameObject.name);
+				if(ray.collider.GetComponent<SpriteRenderer>().color == Color.black)
+					ray.collider.GetComponent<SpriteRenderer>().color = Color.white;
+				else
+					ray.collider.GetComponent<SpriteRenderer>().color = Color.black;
+			}
+		}
+
+		prevPos = currentPos;
+	}
+}
